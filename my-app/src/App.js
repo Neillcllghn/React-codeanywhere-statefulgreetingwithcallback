e85d7d14eb34e375c7cb0934918c61072f131657
Neillcllghn <>
2023-05-15 18:39:11 +0000

    setState Callback function to console

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StatefulGreetingWithCallBack from "./components/StatefulGreetingWithCallBack";
+
+function App() {
+    return (
+        <div className="App">
+            <StatefulGreetingWithCallBack />
+        </div>
+    );
+}
+
+export default App;
