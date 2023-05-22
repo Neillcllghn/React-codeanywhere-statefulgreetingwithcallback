c5ba1674dce308995123792f91bdd9d2923c8c33
Neillcllghn <>
2023-05-22 17:08:00 +0000

    ES7 Snippets and notes

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,62 @@
+
+// Type in rfce + Tab button and the above will be created
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// Type in rfce + Tab button and the above will be created
+
+
+
+// type in rafce + Tab button (creates an arrow function)
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// type in rce + Tab button (class component)
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+// rconst + tab button (constructor)
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// clg + Tab (console log)
+
+// console.log(first)
+
+
+// clo + Tab (console log)
+
+// console.log('first', first)
